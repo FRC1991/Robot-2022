350d20921d8e730a4f6195c61c75cd2be0e3e3a5
mineable3 <97559052+mineable3@users.noreply.github.com>
2022-02-18 18:48:43 -0500

    Added intake motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -9,8 +14,25 @@ public class Intake extends SubsystemBase {
   public static DigitalInput proximitySensor;
   // TODO: add motors
 
+  private final CANSparkMax intakeMotor1, intakeMotor2;
+
+
   public Intake() {
     proximitySensor = new DigitalInput(Constants.proximitySensorDIOIndex);
+
+    intakeMotor1 = new CANSparkMax(99, MotorType.kBrushless);
+    intakeMotor2 = new CANSparkMax(98, MotorType.kBrushless);
+
+    
+  }
+
+
+  public void IntakeMotor1(double speed) {
+   intakeMotor1.set(speed);
+  }
+
+  public void IntakeMotor2(double speed) {
+    intakeMotor2.set(speed);
   }
 
   public boolean isBallIn() {
