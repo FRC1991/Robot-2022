814036f4f6bcfe860d28fa274587946ade78eafa
Jay Kean - UHSSE 4-H FIRST Robotics Team 1991 <40808381+sunfisher62@users.noreply.github.com>
2022-03-15 18:49:35 -0400

    spotlessApply

--- src/main/java/frc/robot/commands/BackupAutoShort.java
+++ src/main/java/frc/robot/commands/BackupAutoShort.java
@@ -1,16 +1,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import java.util.function.Supplier;
 
 public class BackupAutoShort extends SequentialCommandGroup {
   public BackupAutoShort(Supplier<Double> xSteerSupplier, Supplier<Double> yDistanceSupplier) {
     addCommands(
-      new SetShooterPID(() -> ((2100.) + (Math.abs(RobotContainer.yDistance) * 41.3)), () -> (2000.)).withTimeout(3),
-      new FeedBallToShooter().withTimeout(0.5),
-      new DriveDistance(7, -0.9)
+        new SetShooterPID(
+                () -> ((2100.) + (Math.abs(RobotContainer.yDistance) * 41.3)), () -> (2000.))
+            .withTimeout(3),
+        new FeedBallToShooter().withTimeout(0.5),
+        new DriveDistance(7, -0.9)
         // new BallChase(() -> (RobotContainer.ballXError), () -> (RobotContainer.isChasingBall))
         // new AimTurret(xSteerSupplier, yDistanceSupplier)
         );
