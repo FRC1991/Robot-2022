69f5da035c32842a72d9fdcce7d0482c103ec69b
Aryamaan <62923164+arya1106@users.noreply.github.com>
2022-03-29 18:22:41 -0400

    General Cleanup

--- src/main/java/frc/robot/commands/AutoCommands/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/AutoCommands/TwoBallAuto.java
@@ -4,9 +4,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.DrivetrainCommands.BallChase;
 import frc.robot.commands.DrivetrainCommands.DriveDistance;
-import frc.robot.commands.DrivetrainCommands.DriveDistanceAndCapture;
+import frc.robot.commands.DrivetrainCommands.DriveDistanceUntilCapture;
 import frc.robot.commands.IntakeCommands.FeedBallToShooter;
-import frc.robot.commands.IntakeCommands.RunIntakeForBall;
 import frc.robot.commands.ShooterCommands.SetShooterPID;
 import frc.robot.commands.TurretCommands.AimTurret;
 
@@ -16,19 +15,19 @@ public class TwoBallAuto extends SequentialCommandGroup {
   public TwoBallAuto(Supplier<Double> xSteerSupplier, Supplier<Double> yDistanceSupplier, Supplier<Double> targetXErrorSupplier) {
     addCommands(
       new SetShooterPID(
-            () -> ((0.0146*Math.pow(Math.abs(yDistanceSupplier.get()),3)-(0.2013*Math.pow(Math.abs(yDistanceSupplier.get()),2))+(27.232*Math.abs(yDistanceSupplier.get()))+1972.8)),
+            () -> (SetShooterPID.rangeWithLimelight(yDistanceSupplier)),
             () -> (2000.)
             ).withTimeout(2.3),
       new FeedBallToShooter().withTimeout(0.5),
-      // new DriveDistance(7, -0.9),
-      // new RunIntakeForBall(),
-      new DriveDistanceAndCapture(10, -0.9),
+      new DriveDistanceUntilCapture(10, -0.9),
       new SetShooterPID(
-            () -> ((0.0146*Math.pow(Math.abs(yDistanceSupplier.get()),3)-(0.2013*Math.pow(Math.abs(yDistanceSupplier.get()),2))+(27.232*Math.abs(yDistanceSupplier.get()))+1972.8)),
+            () -> (SetShooterPID.rangeWithLimelight(yDistanceSupplier)),
             () -> (2000.)
             ).withTimeout(1),
       new AimTurret(targetXErrorSupplier).withTimeout(1),
       new FeedBallToShooter().withTimeout(0.5)
       );
   }
+
+ 
 }
